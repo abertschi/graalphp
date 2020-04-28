1cfa9cfd1e6f869fc99172e97f66c572e02ac49e
Andrin Bertschi <abertschi@abertschi.ch>
2020-04-28 04:16:44 +0200

    add build info prop

--- graalphp-parser/test/src/test/java/org/graalphp/parser/RunSrcTests.java
+++ graalphp-parser/test/src/test/java/org/graalphp/parser/RunSrcTests.java
@@ -4,6 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class RunSrcTests {
 
@@ -11,9 +14,24 @@ public class RunSrcTests {
 
     @org.junit.Test
     public static Test suite() {
+
         TestSuite suite = new TestSuite(
                 "compare output with upstream php parser");
-        suite.addTest(new FileTestSuite(new File(TEST_FOLDER)));
+        suite.addTest(new FileTestSuite(new File(getDirectory())));
         return suite;
+    }
+
+    private static String getDirectory() {
+        String res = TEST_FOLDER;
+        try (InputStream input =
+                     Main.class.getClassLoader().getResourceAsStream("build.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            res = prop.getProperty("build.directory");
+            res += "/../" + TEST_FOLDER;
+        } catch (IOException ex) {
+        }
+        System.out.println(res);
+        return res;
     }
 }
