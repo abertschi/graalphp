b42ea7986928d01440e71fe9d116895c830416cc
Andrin Bertschi <abertschi@abertschi.ch>
2020-05-05 20:44:19 +0200

    tests and todos

---
+++ graalphp-language/src/test/java/org/graalphp/parser/ParseSkalarTest.java
@@ -0,0 +1,30 @@
+package org.graalphp.parser;
+
+import org.graalvm.polyglot.Context;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.graalphp.parser.ParseAdditionTest.php;
+
+/**
+ * @author abertschi
+ */
+public class ParseSkalarTest {
+    @Test
+    public void parserSkalar() throws IOException {
+        Context ctx = Context.create("php");
+        Assert.assertSame(1, ctx.eval("php", php("1")).asInt());
+        Assert.assertTrue(1000000000L == ctx.eval("php", php("1000000000")).asLong());
+    }
+
+    @Test
+    public void parserSkalarTooBig() throws IOException {
+        // TODO Test overflow
+        Context ctx = Context.create("php");
+//        Assert.assertSame(1, ctx.eval("php", php("-2147483648;")).asLong());
+    }
+
+
+}
