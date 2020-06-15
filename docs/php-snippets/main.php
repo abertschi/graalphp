aec151277ae8b279615dea1913c222bbfd8f78a7
Andrin Bertschi <abertschi@abertschi.ch>
2020-06-15 12:16:33 +0200

    update design notes for functions

--- docs/php-snippets/main.php
+++ docs/php-snippets/main.php
@@ -19,3 +19,36 @@ test();
 
 main();
 ?>
+
+
+
+<?php
+$foo = 1337;
+
+    function test() {
+        global $foo;
+        echo "test: ";
+        echo $foo;
+    }
+
+    test();
+?>
+
+
+
+<?php
+function main() {
+    $foo = 1337;
+
+    function test() {
+        global $foo;
+        echo "test: ";
+        echo $foo;
+    }
+
+        test();
+}
+
+main() // call main with truffle
+
+?>
--- graalphp-language/src/main/java/org/graalphp/nodes/binary/PhpBinaryNode.java
+++ graalphp-language/src/main/java/org/graalphp/nodes/binary/PhpBinaryNode.java
@@ -3,14 +3,15 @@ package org.graalphp.nodes.binary;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeInfo;
+import org.graalphp.PhpException;
 import org.graalphp.nodes.PhpExprNode;
 
 /**
  * @author abertschi
  */
 @NodeInfo(description = "abstract class for all binary expressions")
-@NodeChild("leftNode")
-@NodeChild("rightNode")
+@NodeChild(value = "leftNode", type = PhpExprNode.class)
+@NodeChild(value = "rightNode", type = PhpExprNode.class)
 public abstract class PhpBinaryNode extends PhpExprNode {
 
 
