9968b937c82cbdc44612383947fabfafbdec1235
Andrin Bertschi <abertschi@abertschi.ch>
2020-06-24 16:43:17 +0200

    fix issue with short circuit logic

---
+++ graalphp-language/tests/test.php
@@ -0,0 +1,13 @@
+<?php
+
+$e = false || 10;
+
+print $e;
+
+// The constant false is assigned to $f before the "or" operation occurs
+// Acts like: (($f = false) or true)
+// $f = false or true;
+
+
+// print $f;
+?>
