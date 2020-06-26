74b6cb0e8591413ec69372a9ad223db531d8ad28
Andrin Bertschi <abertschi@abertschi.ch>
2020-06-26 18:37:20 +0200

    [experiment] make call side of invoke function a directcalltarget

--- graalphp-language/src/main/java/org/graalphp/nodes/function/PhpInvokeNode.java
+++ graalphp-language/src/main/java/org/graalphp/nodes/function/PhpInvokeNode.java
@@ -1,7 +1,10 @@
 package org.graalphp.nodes.function;
 
 import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import org.graalphp.nodes.PhpExprNode;
 import org.graalphp.types.PhpFunction;
@@ -19,17 +22,26 @@ public final class PhpInvokeNode extends PhpExprNode {
     @Children
     protected PhpExprNode[] argNodes;
 
+    @Child
+    @CompilationFinal
+    private DirectCallNode callNode;
+
     // TODO Change this to an abstract nodes, introduce object [] types
     public PhpInvokeNode(PhpExprNode[] arguments, PhpExprNode function) {
         this.argNodes = arguments;
         this.function = function;
+        callNode = null;
     }
 
     @ExplodeLoop
     @Override
     public Object executeGeneric(VirtualFrame frame) {
-        // TODO: improve this, avoid casting
-        PhpFunction fn = (PhpFunction) function.executeGeneric(frame);
+        if (callNode == null) {
+            // TODO: improve this, avoid casting
+            PhpFunction fn = (PhpFunction) function.executeGeneric(frame);
+            this.callNode =  DirectCallNode.create(fn.getCallTarget());
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+        }
 
         // for a single node, number of arguments is constant
         CompilerAsserts.partialEvaluationConstant(argNodes.length);
@@ -39,6 +51,6 @@ public final class PhpInvokeNode extends PhpExprNode {
             argVals[i] = argNodes[i].executeGeneric(frame);
         }
         // TODO: do polymorphic inline cache technique here
-        return fn.getCallTarget().call(argVals);
+        return this.callNode.call(argVals);
     }
 }
