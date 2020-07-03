5e517583bbd975145c8b3ab12b35ea490c574871
Andrin Bertschi <abertschi@abertschi.ch>
2020-07-03 19:04:56 +0200

    introduce ++, -- parsing behavior and initial implementation

---
+++ graalphp-language/tests/prefix-postfix/postfix-assign.php
@@ -0,0 +1,34 @@
+<?php
+// test postfix assign behaviour
+
+$a = 0;
+$c = $a++;
+print $c;
+print $a;
+
+$c = 0;
+$a = 0;
+$c = $a--;
+print $c;
+print $a;
+
+$c = 0;
+$a = 0;
+$c = $a-- + $a++;
+print $c;
+
+$c = 0;
+$a = 0;
+$c = $a++ + $a++;
+print $c;
+
+$c = 0;
+$a = 0;
+$c = $a-- + $a--;
+print $c;
+
+$c = 0;
+$a = 0;
+$c = $a-- + $a-- + $a-- + $a--  + $a-- + $a-- + $a-- + $a--;
+print $c;
+?>
---
+++ graalphp-language/tests/prefix-postfix/simple-prefix.php
@@ -0,0 +1,39 @@
+<?php
+
+// test prefix implementation
+$a = 1;
+
+++$a;
+++$a;
+
+print $a;
+
+
+--$a ;
+--$a ;
+print $a;
+
+$a = 0;
+for($i = 0; $i < 10; ++$i) {
+$a = $a + 1;
+}
+print $a;
+
+$a = 0;
+for($i = 10; $i > 0; --$i) {
+$a = $a + 1;
+}
+print $a;
+
+$a = 0;
+$c = ++$a;
+print $c;
+print $a;
+
+
+$a = 0;
+$c = --$a;
+print $c;
+print $a;
+
+?>
