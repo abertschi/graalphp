c7ae401d966ee5ff2a237acab3d26ff7b777a287
Andrin Bertschi <abertschi@abertschi.ch>
2020-07-20 11:08:38 +0200

    update benchmarks

--- docs/benchmarks/spectralnorm.php-2.orig.php
+++ docs/benchmarks/spectralnorm.php-2.orig.php
@@ -6,24 +6,16 @@ contributed by Isaac Gouy
 modified by anon
 */
 
-/*
-modifications:
- - pass integer by value not by reference
- - pass global variable as argument instead of using global keyword
- - do not print additional text beside result value
- - replace for each by for keyworod
-*/
-
-function A($i, $j){
+function A(&$i, &$j){
     return 1.0 / ( ( ( ($i+$j) * ($i+$j+1) ) >> 1 ) + $i + 1 );
 }
 
-function Av($n, &$v, &$_tpl){
-    $Av = $_tpl; // assign by value
+function Av(&$n,&$v){
+    global $_tpl;
+    $Av = $_tpl;
     for ($i = 0; $i < $n; ++$i) {
         $sum = 0.0;
-        for($j = 0; $j < $n; $j++) {
-            $v_j = $v[$j];
+        foreach($v as $j=>$v_j) {
             $sum += A($i,$j) * $v_j;
         }
         $Av[$i] = $sum;
@@ -31,12 +23,12 @@ function Av($n, &$v, &$_tpl){
     return $Av;
 }
 
-function Atv($n, &$v, &$_tpl){
+function Atv(&$n,&$v){
+    global $_tpl;
     $Atv = $_tpl;
     for($i = 0; $i < $n; ++$i) {
         $sum = 0.0;
-        for($j = 0; $j < $n; $j++) {
-            $v_j = $v[$j];
+        foreach($v as $j=>$v_j) {
             $sum += A($j,$i) * $v_j;
         }
         $Atv[$i] = $sum;
@@ -44,28 +36,26 @@ function Atv($n, &$v, &$_tpl){
     return $Atv;
 }
 
-function AtAv($n, &$v, &$_tpl){
-    $tmp = Av($n,$v, $_tpl); 
-    return Atv($n, $tmp, $_tpl);
+function AtAv(&$n,&$v){
+    $tmp = Av($n,$v);
+    return Atv($n, $tmp);
 }
 
 $n = intval(($argc == 2) ? $argv[1] : 1);
-
 $u = array_fill(0, $n, 1.0);
 $_tpl = array_fill(0, $n, 0.0);
 
 for ($i=0; $i<10; $i++){
-    $v = AtAv($n,$u, $_tpl);
-    $u = AtAv($n,$v, $_tpl);
+    $v = AtAv($n,$u);
+    $u = AtAv($n,$v);
 }
 
 $vBv = 0.0;
 $vv = 0.0;
-
-for($i = 0; $i < $n; $i ++) {
-    $val = $v[$i];
+$i = 0;
+foreach($v as $val) {
     $vBv += $u[$i]*$val;
     $vv += $val*$val;
+    ++$i;
 }
-
 printf("%0.9f\n", sqrt($vBv/$vv));
