7b8d9c584ca5b9761663e532a8e4b552b4958687
Andrin Bertschi <abertschi@abertschi.ch>
2020-07-26 19:02:02 +0200

    introduce impl. for array fill

--- graalphp-language/src/main/java/org/graalphp/builtins/language/ArrayFillBuiltin.java
+++ graalphp-language/src/main/java/org/graalphp/builtins/language/ArrayFillBuiltin.java
@@ -2,11 +2,13 @@ package org.graalphp.builtins.language;
 
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.library.CachedLibrary;
+import com.oracle.truffle.api.nodes.NodeInfo;
 import org.graalphp.builtins.PhpBuiltinNode;
 import org.graalphp.exception.ArrayCapacityExceededException;
 import org.graalphp.runtime.array.ArrayFactory;
 import org.graalphp.runtime.array.ArrayLibrary;
 import org.graalphp.runtime.array.LongArrayAllocator;
+import org.graalphp.runtime.array.ObjectArrayAllocator;
 import org.graalphp.runtime.array.PhpArray;
 
 /**
@@ -17,14 +19,47 @@ import org.graalphp.runtime.array.PhpArray;
  *
  * @author abertschi
  */
+@NodeInfo(shortName = ArrayFillBuiltin.NAME)
 public abstract class ArrayFillBuiltin extends PhpBuiltinNode {
+
+    public static final String NAME = "array_fill";
 
     @Specialization
     protected PhpArray arrayFillLong(long startIndex, long num, long val,
                                      @CachedLibrary(limit = "1") ArrayLibrary libs) {
-        int arraySize = toInt(val);
+        int arraySize = toInt(num);
         Object backend = LongArrayAllocator.ALLOCATOR.allocate(arraySize);
         PhpArray phpArray = ArrayFactory.newArray(backend, arraySize);
+
+        for (int i = 0; i < arraySize; i++) {
+            libs.write(backend, i, val);
+        }
+        return phpArray;
+    }
+
+    @Specialization
+    protected PhpArray arrayFillDouble(long startIndex, long num, double val,
+                                       @CachedLibrary(limit = "1") ArrayLibrary libs) {
+        int arraySize = toInt(num);
+        Object backend = ArrayLibrary.getAllocatorForValue(val);
+        PhpArray phpArray = ArrayFactory.newArray(backend, arraySize);
+
+        for (int i = 0; i < arraySize; i++) {
+            libs.write(backend, i, val);
+        }
+        return phpArray;
+    }
+
+    @Specialization
+    protected PhpArray arrayFillObject(long startIndex, long num, Object val,
+                                       @CachedLibrary(limit = "1") ArrayLibrary libs) {
+        int arraySize = toInt(num);
+        Object backend = ObjectArrayAllocator.ALLOCATOR.allocate(arraySize);
+        PhpArray phpArray = ArrayFactory.newArray(backend, arraySize);
+
+        for (int i = 0; i < arraySize; i++) {
+            libs.write(backend, i, val);
+        }
         return phpArray;
     }
 
